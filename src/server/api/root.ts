689ccb13b1bbee6c28d791908192a4c9f1bb90fd
Sam <samueljoh329@gmail.com>
2023-04-14 14:29:23 -0500

    setup prisma models

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { accountsRouter } from "./routers/accounts";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  accounts: accountsRouter,
 });
 
 // export type definition of API
